1dc16f7af36f7d6ab84869b20c4a2beec09c6535
grigorii_pokatilo <zilibobin.tik.tok@gmail.com>
2025-01-01 18:10:57 +0200

    CB-0: added SpringBoot skeleton.

---
+++ src/main/java/com/github/hphub/cb/CryptoTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.hphub.cb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoTelegramBotApplication.class, args);
+	}
+
+}
